65614aa9a0d633091ad9736b074104553a7adb30
homebase3 <65870950+homebase3@users.noreply.github.com>
2021-07-09 17:38:14 -0700

    Initial data commit with analysis.py

---
+++ analysis.py
@@ -0,0 +1,13 @@
+# %% md
+
+# Title
+
+# %% codecell
+import numpy as np
+import pandas as pd
+import rpy2
+
+# %% codecell
+
+train = pd.read_csv('data/train.csv')
+test = pd.read_csv('data/test.csv')
